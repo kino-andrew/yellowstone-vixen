f0136acda78d6613c76f8a9d23e81067c23f8efc
Orex <orex@raccoons.dev>
2026-02-11 17:49:15 +0100

    nits

--- crates/proc-macro/src/utils.rs
+++ crates/proc-macro/src/utils.rs
@@ -1,20 +1,20 @@
-pub fn to_snake_case(s: &str) -> String {
-    let mut out = String::with_capacity(s.len());
+pub fn to_snake_case(str: &str) -> String {
+    let mut out = String::with_capacity(str.len());
 
-    for (i, ch) in s.chars().enumerate() {
-        if ch.is_uppercase() && i != 0 {
+    for (i, char) in str.chars().enumerate() {
+        if char.is_uppercase() && i != 0 {
             out.push('_');
         }
 
-        out.push(ch.to_ascii_lowercase());
+        out.push(char.to_ascii_lowercase());
     }
 
     out
 }
 
-pub fn to_pascal_case(s: &str) -> String {
-    if let Some((first, rest)) = s.chars().next().map(|first| (first, &s[1..])) {
-        let mut result = String::with_capacity(s.len());
+pub fn to_pascal_case(str: &str) -> String {
+    if let Some((first, rest)) = str.chars().next().map(|first| (first, &str[1..])) {
+        let mut result = String::with_capacity(str.len());
 
         result.push(first.to_ascii_uppercase());
         result.push_str(rest);
